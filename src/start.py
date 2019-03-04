39cadbaf38465bc518fae7d4075abd0d8ad3b4e9
rgsl888 <rgsl888@tamu.edu>
2019-03-03 20:53:46 -0600

    Saves to CSV

--- src/start.py
+++ src/start.py
@@ -4,6 +4,8 @@ import csv
 import os
 import pickle
 import random
+import pandas as pd
+import time
 
 
 from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox
@@ -20,9 +22,6 @@ class Login(QDialog):
         loadUi('../gui/login.ui', self)
         self.okButton.clicked.connect(self.login)
         self.exitButton.clicked.connect(QCoreApplication.instance().quit)
-        
-        
-
     
     def login(self):
         global userName
@@ -37,7 +36,7 @@ class MainWindow(QDialog):
         self.setWindowTitle("Activity Recorder")
         self.setWindowIcon(QIcon('../resources/icon.png'))
 
-        self.username.setText(username)
+        self.username.setText("User - "+ username)
         self.user = username
         self.exitButton.clicked.connect(self.quit)
         self.webcamEnabled = False
@@ -49,10 +48,28 @@ class MainWindow(QDialog):
         self.timer = QTimer(self)
         #self.timer.timeout
         self.timer.timeout.connect(self.updateFrame)
+        self.data_path = "../userdata/data.csv"
+        if not os.path.exists(self.data_path): 
+            self.data = pd.DataFrame(
+                {
+                    "username"  : [],
+                    "label1"    : [],
+                    "label2"    : [],
+                    "true_label": [],
+                    "user_choice"   : [],
+                    "session_video" : []
+
+                }
+            ) 
+        else:
+           self.data = pd.read_csv (self.data_path); 
+        self.started = False
     
     def quit(self):
         if self.webcamEnabled == True:
             self.stopWebCam()
+
+        self.data.to_csv (self.data_path, index=False)
         
         result = QMessageBox.question(self, 'Message', "Are you sure?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
         if result == QMessageBox.Yes:
@@ -72,6 +89,19 @@ class MainWindow(QDialog):
 
 
     def next(self):
+        if self.started == False:
+            self.started = True
+        else:
+            correct_choice = 0
+            if ((self.ans1.isChecked ()) and (self.ans == 1)):
+                correct_choice = 1
+                
+            elif((self.ans2.isChecked ()) and (self.ans == 2)):
+                correct_choice = 1
+
+            self.data=self.data.append(pd.DataFrame ([[self.user, self.label1, self.label2, self.true_label, correct_choice, self.output_filename]], columns=['username', 'label1', 'label2', 'true_label', 'user_choice', 'session_video']), ignore_index=True, sort=False)
+            
+            
         if self.webcamEnabled == True:
             self.stopWebCam()
         self.nextButton.setText("Next")
@@ -84,27 +114,27 @@ class MainWindow(QDialog):
         self.pic1.setPixmap(image1)
         self.pic2.setPixmap(image2)
 
-        label1 = image_categories[0][10:].replace('_',' ')
-        label2 = image_categories[1][10:].replace('_',' ')
+        self.label1 = image_categories[0][10:].replace('_',' ')
+        self.label2 = image_categories[1][10:].replace('_',' ')
 
         if random.randint(0,1):
-            self.a1.setText(label1)
-            self.b2.setText(label1)
-            self.a2.setText(label2)
-            self.b1.setText(label2)
+            self.label.setText("LABEL : " + self.label1)
+            self.ans = 1;
+            self.true_label = self.label1
         else:
-            self.a1.setText(label2)
-            self.b2.setText(label2)
-            self.a2.setText(label1)
-            self.b1.setText(label1)
+            self.label.setText("LABEL : " + self.label2)
+            self.ans = 2;
+            self.true_label = self.label2
 
         if self.webcamEnabled == False:
             self.webcamEnabled = True
             self.savedir = "../userdata/videos/"+self.user
+       
             if not os.path.exists(self.savedir):
                 os.makedirs(self.savedir)
             fourcc = cv.VideoWriter_fourcc('X','V','I','D')
-            self.outputfile = self.savedir+ "/" +image_categories[0]+"+" + image_categories[1] + ".avi"
+            self.output_filename = time.strftime("%Y%m%d_%H%M%S")+ ".avi"
+            self.outputfile = self.savedir+ "/" + self.output_filename
             self.out = cv.VideoWriter(self.outputfile, fourcc, 30.0, (640,480))
             self.capture = cv.VideoCapture(0)
             self.capture.set(cv.CAP_PROP_FRAME_WIDTH, 640)
